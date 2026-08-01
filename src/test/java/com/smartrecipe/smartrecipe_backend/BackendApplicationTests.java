c0931f5863e07ca6b7fdb1f5d25834da6cc54c9f
NHTung-0801 <kiritohackiem05@gmail.com>
2026-08-01 21:46:14 +0700

    Initialize Spring Boot backend structure

---
+++ src/test/java/com/smartrecipe/smartrecipe_backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartrecipe.smartrecipe_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
